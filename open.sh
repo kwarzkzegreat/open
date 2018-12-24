d59b7130bec97433c67f4fbabd5da5e792095ac9
kwarzkzegreat <m.p.kirchner@gmx.de>
2018-12-24 10:27:58 +0100

    added .PDF ext

--- open.sh
+++ open.sh
@@ -1,17 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 open() {
 if [ -z "$1" ]; then
-    echo "open what, dummy?"
+    printf 'open what, dummy?\n'
 else
-  if [[ "$1" == *.pdf ]]; then
+  if [[ "$1" == *.pdf ]] \
+      || [[ "$1" == *.PDF ]]; then
       mupdf "$1"
   else
       if [[  "$1" == *.jpg ]] \
-      || [[ "$1" == *.png ]] \
-      || [[ "$1" == *.jpeg ]] \
-      || [[ "$1" == *.gif ]]; then
-    display -quiet -size 320x180 "$1"
+        || [[ "$1" == *.JPG ]] \
+        || [[ "$1" == *.png ]] \
+        || [[ "$1" == *.jpeg ]] \
+        || [[ "$1" == *.gif ]]; then
+            display \
+                -quiet \
+                -size 320x180 "$1"
       else
           find -path "*$1*" -ok exo-open {} \;
       fi
